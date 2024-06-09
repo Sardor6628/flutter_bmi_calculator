8cea9cf658018c11a471d3b47a325ef428cccc11
Sardor6628 <sardor.dsu@gmail.com>
2024-06-09 17:49:12 +0900

    - final touches

--- lib/business_logic/bmi_calculator_bl.dart
+++ lib/business_logic/bmi_calculator_bl.dart
@@ -1,13 +1,14 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_bmi_calculator/constants/constants.dart';
 import 'package:flutter_bmi_calculator/model/bmi_representation_model.dart';
 import 'package:get/get.dart';
+import 'package:share/share.dart';
 
 class BmiCalculatorController extends GetxController {
   Rx<BmiCalculatorModel> bmiCalculatorModel =
       BmiCalculatorModel(height: 100, weight: 40, age: 16, isMale: true).obs;
 
   void updateHeight(double height) {
-
     if (height < MIN_HEIGHT || height > MAX_HEIGHT) {
       return;
     }
@@ -38,5 +39,23 @@ class BmiCalculatorController extends GetxController {
     bmiCalculatorModel.update((val) {
       val!.isMale = isMale;
     });
+  }
+
+  void share(BuildContext context) {
+    final strShareMessage =
+        'Hello!! my BMI is ${bmiCalculatorModel.value.calculateBmi()}\n\n' +
+            'my current condition: ${bmiCalculatorModel.value.checkAdult()}\n' +
+            'my age is: ${bmiCalculatorModel.value.age}\n\n' +
+            'App suggested me that: ${bmiCalculatorModel.value.getSuggestions()}';
+
+    try {
+      final RenderBox box = context.findRenderObject() as RenderBox;
+      Share.share(strShareMessage,
+          subject: 'Share BMI Info',
+          sharePositionOrigin: box.localToGlobal(Offset.zero) & box.size);
+    } catch (e) {
+      final snackBar = SnackBar(content: Text(e.toString()));
+      ScaffoldMessenger.of(context).showSnackBar(snackBar);
+    }
   }
 }
--- lib/presentation/bmi_results_page.dart
+++ lib/presentation/bmi_results_page.dart
@@ -24,67 +24,116 @@ class BmiResultsPage extends StatelessWidget {
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
-            Container(
-              decoration: BoxDecoration(
-                color: Color(0xFF1E2138),
-                borderRadius: BorderRadius.circular(16),
-              ),
-              padding: EdgeInsets.all(16),
-              child: Column(
+            Column(
+              children: [
+                Container(
+                  decoration: BoxDecoration(
+                    color: Color(0xFF1E2138),
+                    borderRadius: BorderRadius.circular(16),
+                  ),
+                  padding: EdgeInsets.all(16),
+                  child: Column(
+                    children: [
+                      Text(
+                        controllerGet.bmiCalculatorModel.value.checkAdult(),
+                        style: TextStyle(
+                          color: Colors.green,
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      SizedBox(height: 10),
+                      Text(
+                        controllerGet.bmiCalculatorModel.value
+                            .calculateBmi()
+                            .toStringAsFixed(1),
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 48,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      SizedBox(height: 10),
+                      Text("Normal BMI range:",
+                          style: TextStyle(color: Colors.grey)),
+                      SizedBox(height: 10),
+                      Text(
+                        "18.5 - 25 kg/m2",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 16,
+                        ),
+                      ),
+                      SizedBox(height: 10),
+                      Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              'Your age: ',
+                              style: TextStyle(color: Colors.red),
+                            ),
+                            Text(
+                              "${controllerGet.bmiCalculatorModel.value.age.toString()} (${controllerGet.bmiCalculatorModel.value.isMale ? "Male" : "Female"})",
+                              style: TextStyle(color: Colors.white),
+                            ),
+                          ]),
+                    ],
+                  ),
+                ),
+                SizedBox(height: 20),
+                Container(
+                    width: double.infinity,
+                    padding: EdgeInsets.all(16),
+                    decoration: BoxDecoration(
+                      color: Color(0xFF1E2138),
+                      borderRadius: BorderRadius.circular(16),
+                    ),
+                    child: RichText(
+                      text: TextSpan(
+                        text: "Suggestions: ",
+                        style: TextStyle(
+                          color: Colors.red,
+                          fontSize: 16,
+                        ),
+                        children: [
+                          TextSpan(
+                            text: controllerGet.bmiCalculatorModel.value
+                                .getSuggestions(),
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 16,
+                            ),
+                          ),
+                        ],
+                      ),
+                    )),
+              ],
+            ),
+            Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: [
-                  Text(
-                    controllerGet.bmiCalculatorModel.value.checkAdult(),
-                    style: TextStyle(
-                      color: Colors.green,
-                      fontSize: 16,
-                      fontWeight: FontWeight.bold,
-                    ),
+              Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    border: Border.all(color: Colors.red),
                   ),
-                  SizedBox(height: 10),
-                  Text(
-                  controllerGet.bmiCalculatorModel.value.calculateBmi().toStringAsFixed(1),
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 48,
-                      fontWeight: FontWeight.bold,
-                    ),
+                  child: IconButton(
+                      onPressed: () {
+                        controllerGet.share(context);
+                      }, icon: const Icon(Icons.share))),
+              Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    border: Border.all(color: Colors.red),
                   ),
-                  SizedBox(height: 10),
-                  Text("Normal BMI range:", style: TextStyle(color: Colors.grey)),
-                  SizedBox(height: 10),
-                  Text(
-                    "18.5 - 25 kg/m2",
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 16,
-                    ),
-                  ),
-                  SizedBox(height: 10),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children:[
-                      Text('Your age: ', style: TextStyle(color: Colors.red),),
-                      Text("${controllerGet.bmiCalculatorModel.value.age.toString()} (${controllerGet.bmiCalculatorModel.value.isMale?"Male":"Female"})", style: TextStyle(color: Colors.white),),
-                    ]
-                  ),
-                ],
-              ),
-            ),
-            SizedBox(height: 20),
-            Container(
-
-            ),
-            ElevatedButton(
-              onPressed: () {
-                controllerGet.updateHeight(100);
-                controllerGet.updateWeight(40);
-                controllerGet.updateAge(16);
-                controllerGet.updateGender(true);
-                Get.offAll(() => BMICalculatorPage());
-              },
-              child: Text("Recalculate"),
-            ),
+                  child: IconButton(
+                      onPressed: () {
+                        Get.offAll(() => BMICalculatorPage());
+                      },
+                      icon: const Icon(Icons.refresh))),
+            ])
           ],
         ),
       ),
